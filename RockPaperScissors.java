cfdba583380be714c0875c5ec73d3d485e133db9
Stavromir <s.kasapov@protonmail.com>
2023-03-21 22:44:17 +0200

    if statement for player choice complete

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -8,7 +8,7 @@ public class RockPaperScissors {
         final String paper = "Paper";
         final String scissors = "Scissors";
 
-        System.out.printf("Chose [R]ock, [P]aper, [S]cissors: ");
+        System.out.println("Chose [R]ock, [P]aper, [S]cissors: ");
 
         String input = scanner.nextLine();
 
@@ -16,6 +16,13 @@ public class RockPaperScissors {
 
         if (input.equals("r") || input.equals("R") || input.equals("rock") || input.equals("Rock")) {
             playerMove = rock;
+        } else if (input.equals("p") || input.equals("P") || input.equals("paper") || input.equals("Paper")) {
+            playerMove = paper;
+        } else if (input.equals("s") || input.equals("S") || input.equals("scissors") || input.equals("Scissors")) {
+            playerMove = scissors;
+        } else {
+            System.out.println("Invalid input. Please try again...");
+            return;
         }
 
 
