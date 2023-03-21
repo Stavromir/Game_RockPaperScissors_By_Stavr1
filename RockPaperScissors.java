d137923787a5c067ebb5e4bad76ceaf397257451
Stavromir <s.kasapov@protonmail.com>
2023-03-21 22:25:32 +0200

    Print options and read input

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -4,6 +4,18 @@ public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        final String ROCK = "Rock";
+        final String PAPER = "Paper";
+        final String SCISSORS = "Scissors";
+
+        System.out.printf("Chose [R]ock, [P]aper, [S]cissors:");
+
+        String input = scanner.nextLine();
+
+
+
+
+
 
     }
 }
