4b84bfab7d3a8354eef5b7d9933138465c8c7d67
Stavromir <s.kasapov@protonmail.com>
2023-03-21 22:36:20 +0200

    string changes and if statement

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -4,15 +4,19 @@ public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        final String ROCK = "Rock";
-        final String PAPER = "Paper";
-        final String SCISSORS = "Scissors";
+        final String rock = "Rock";
+        final String paper = "Paper";
+        final String scissors = "Scissors";
 
-        System.out.printf("Chose [R]ock, [P]aper, [S]cissors:");
+        System.out.printf("Chose [R]ock, [P]aper, [S]cissors: ");
 
         String input = scanner.nextLine();
 
+        String playerMove = "";
 
+        if (input.equals("r") || input.equals("R") || input.equals("rock") || input.equals("Rock")) {
+            playerMove = rock;
+        }
 
 
 
